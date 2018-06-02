a406553f8bb2d777cce5c09371eaad80bcf36f48
CrypticDesign <mashoobee@gmail.com>
2018-06-01 17:29:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,53 +1,46 @@
-const Discord = require('discord.js');
-const fs = require('fs'); 
+const Discord = require("discord.js");
+const botconfig = require("./botconfig.json");
+const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
-const botconfig = require("./botconfig.json"); 
 bot.commands = new Discord.Collection();
 
-fs.readdir("commands/", (err, files) => {
-  
-  if(err) console.log(err)
-  
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.log(err);
+
   let jsfile = files.filter(f => f.split(".").pop() === "js")
   if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
+    console.log("Couldn't find commands");
     return;
   }
-  
+
   jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded`);
-    bot.commands.set(props.help.name, props);
-  });
-  
+  let props = require(`./commands/${f}`);
+  console.log(`${f} loaded!`);
+  bot.commands.set(props.help.name, props);
+    });
+
 });
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`); 
-  bot.user.setActivity ("Playstation Grenade!", {type: "WATCHING"});
 
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is now online`);
+  bot.user.setActivity("Playstation Grenade!", {type: "WATCHING"});
 });
 
 bot.on("message", async message => {
-  
   if(message.author.bot) return;
-  if(message.channel.type === "dm") return; 
+  if(message.channel.type === "dm") return;
+
   
   let prefix = botconfig.prefix;
-  let messageArray = message.content.split(' ');
+  if(!message.content.startsWith(prefix)) return;
+  let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
-  let args = messageArray.slice(1); 
-  
+  let args = messageArray.slice(1);
+
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
-    
-  if(cmd === `${prefix}help`) {
-    let helpembed = new Discord.RichEmbed()
-    .setDescription("Test!");
-      
-    return message.channel.send(helpembed);
-  }
-  
+if(commandfile) commandfile.run(bot, message, args);
+
 });
 
-bot.login(process.env.TOKEN);
+bot.login(process.env.BOT_TOKEN);
